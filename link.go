556bbcca6f0cb8b2be1d1b58d5122786936e1477
gldeng <realgldeng@gmail.com>
2022-05-11 10:50:03 +0800

    Fix build on windows

--- link.go
+++ link.go
@@ -1,6 +1,7 @@
 package bls_tss
 /*
 #cgo LDFLAGS:-lbls_tss -lm -ldl
+#cgo windows,amd64 LDFLAGS: -lws2_32 -luserenv -lbcrypt
 #cgo linux,amd64 LDFLAGS:-L${SRCDIR}/bls-tss/lib/linux/amd64
 #cgo linux,arm64 LDFLAGS:-L${SRCDIR}/bls-tss/lib/linux/arm64
 #cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/bls-tss/lib/darwin/amd64
